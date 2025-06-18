a50915237899d98cc6df65111546babef321872b
Skyron9 <courtier.leo@gmail.com>
2025-06-18 22:06:04 +0200

    Exercice 1 - Magic number

---
+++ BlazorApp1/Components/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components.Pages;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+
+    [Parameter]
+    public int InitialValue {
+        get => currentCount;
+        set => currentCount = value;
+    }
+    private void IncrementCount() {
+        currentCount++;
+    }
+}
---
+++ BlazorApp1/Components/Pages/MagicNumber.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace BlazorApp1.Components.Pages;
+
+public partial class MagicNumber
+{
+    private int _userInputValue = 0;
+    private int _magicNumberValue = 0;
+    private const int MagicNumberMinValue = 0;
+    private const int MagicNumberMaxValue = 20;
+
+    private int _lifes = 5;
+
+    private int Lifes {
+        get => _lifes;
+        set => _lifes = value;
+    }
+
+    private string _responseToUser = "";
+
+    protected override void OnInitialized() {
+        base.OnInitialized();
+        var random = new Random();
+        _magicNumberValue = random.Next(MagicNumberMinValue, MagicNumberMaxValue);
+    }
+
+    private void CheckResponse() {
+        if (_userInputValue == _magicNumberValue) {
+            Win();
+        }
+        else {
+            Lifes--;
+            if(Lifes <=0) Lose();
+        }
+    }
+
+    private void Win() {
+        _responseToUser =$"Congratulations ! You guessed the magic number : {_magicNumberValue}!";
+    }
+
+    private void Lose()
+    {
+        _responseToUser = "You have no more life, game over...";
+    }
+}
