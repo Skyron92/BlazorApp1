f29430265e8aff978db3a5d3a76ee923aace0f5d
Skyron <courtier.leo@gmail.com>
2025-06-22 13:32:18 +0200

    Password implementation

---
+++ BlazorApp1/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorApp1.Components.Layout;
+
+public partial class NavMenu {
+    private struct NavMenuItem {
+        public NavMenuItem(string label, string route, string icon) {
+            Label = label;
+            Route = route;
+            Icon = icon;
+        }
+        public string Label { get; }
+        public string Route { get; }
+        public string Icon { get; }
+    }
+
+    private NavMenuItem[] NavMenuItems = [
+        new NavMenuItem("Home", "","bi-house-door-fill-nav-menu"),
+        new NavMenuItem("Counter", "counter","bi-plus-square-fill-nav-menu"),
+        new NavMenuItem("Weather", "weather","bi-list-nested-nav-menu"),
+        new NavMenuItem("Magic number", "magicNumber","bi-plus-square-fill-nav-menu"),
+        new NavMenuItem("Drag and drop", "dragAndDrop","bi-list-nested-nav-menu"),
+        new NavMenuItem("Protected area", "protected","bi-list-nested-nav-menu")
+    ];
+}
---
+++ BlazorApp1/Components/Pages/ProtectedArea.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components.Pages;
+
+public partial class ProtectedArea
+{
+    private string _userInput="";
+    private const string Password = "WxB9!4";
+    private string _passwordResponse = "";
+
+    private void ChangePassword(ChangeEventArgs e) {
+        _userInput = e.Value?.ToString() ?? string.Empty;
+        _passwordResponse = _userInput == Password ? "" : "Invalid password.";
+    }
+}
